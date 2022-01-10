c218db908fadc2f30eacb68f4796596c07d5ac62
Bernat <bernattorras@gmail.com>
2022-01-10 12:34:43 +0100

    Initial plugin scaffold

---
+++ wp-disable-inactive-users.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:     Wp Disable Inactive Users
+ * Plugin URI:      https://github.com/bernattorras/wp-disable-inactive-users
+ * Description:     A WordPress plugin that disables inactive users automatically.
+ * Author:          Bernat Torras
+ * Text Domain:     wp-disable-inactive-users
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Disable_Inactive_Users
+ */
+
+// Your code starts here.
