20789285851970f14842e8b4e0db09276b9d18eb
Bernat <bernattorras@gmail.com>
2022-01-10 14:47:08 +0100

    Initial plugin classes with a test  validation

---
+++ includes/User.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace WPDIU;
+
+/**
+ * User class
+ * This class handles all the user functionlity.
+ */
+class User {
+
+	/**
+	 * Checks if the user that is trying to log in is inactive.
+	 *
+	 * @param WP_User $user - The current user object.
+	 * @param string  $password - The password of the user.
+	 * @return WP_User $user or WP_Error - The current user object if the user is active. A WP_Error otherwise.
+	 */
+	public static function check_if_user_active( \WP_User $user, string $password ) {
+
+		$status = get_user_meta( $user->ID, 'user_status' );
+
+		if ( self::is_user_active( $user ) ) {
+			return self::throw_inactive_error( $user );
+		}
+
+		return $user;
+	}
+
+	/**
+	 * Checks if the provided user is active.
+	 *
+	 * @param \WP_User $user - The requested user.
+	 * @return boolean - True if the user is active. False otherwise.
+	 */
+	public static function is_user_active( \WP_User $user ) {
+		return true;
+	}
+
+	/**
+	 * Returns a WP_Error saying that the user is inactive.
+	 *
+	 * @param WP_User $user - The current user object.
+	 * @return WP_Error - An error saying that the user is inactive.
+	 */
+	public static function throw_inactive_error( $user ) {
+
+		$username = $user->user_login;
+		$wpdiu    = new \WPDIU();
+
+		return new \WP_Error(
+			'inactive_user',
+			sprintf(
+				/* translators: %1$s: User's username. %2$s: The days limit. */
+				__( '<strong>Error</strong>: The username <strong>%1$s</strong> has been disabled because it has been inactive for %2$s days.' ),
+				$username,
+				$wpdiu::$days_limit
+			)
+		);
+	}
+}
--- wp-disable-inactive-users.php
+++ wp-disable-inactive-users.php
@@ -11,4 +11,45 @@
  * @package         Wp_Disable_Inactive_Users
  */
 
-// Your code starts here.
+/**
+ * TODO:
+ * - Guardar la data actual com a user_meta al fer login (last_login)
+ * - Revisar la data al fer login. Retornar error si fa mes de $90 dies de l'ultim login.
+ * -
+ * EXTRA FEATURES
+ * - Opció per no desactivar admins
+ * - Opció per no desactivar usuaris seleccionats (user meta o selector d'usuaris?)
+ * - Llistat d'usuaris deshabilitats
+ * - Enviar un email a l'admin quan es desactivi un usuari
+ * - Enviar un email recordatori X dies abans de desactivar un usuari
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * WPDIU class
+ */
+class WPDIU {
+
+	/**
+	 * Contains errors and messages as admin notices.
+	 *
+	 * @var array
+	 */
+	public static $days_limit = 90;
+
+	/**
+	 * WPDIU Init
+	 */
+	public static function init() {
+
+		// Composer autoload.
+		require_once __DIR__ . '/vendor/autoload.php';
+
+		// Validate the user to check if it's active.
+		add_action( 'wp_authenticate_user', [ 'WPDIU\User', 'check_if_user_active' ], 10, 2 );
+	}
+
+}
+
+WPDIU::init();
