2fb154dddb9a074d8bcec5d3382edca994eebb14
Bernat <bernattorras@gmail.com>
2022-01-12 11:21:43 +0100

    Create and delete test user at start and test end

--- tests/test-user.php
+++ tests/test-user.php
@@ -5,8 +5,6 @@
  * @package Wp_Disable_Inactive_Users
  */
 
-use WP_Error;
-
 /**
  * User test case.
  */
@@ -26,13 +24,62 @@ class UserTest extends WP_UnitTestCase {
 	 */
 	public static $user_class;
 
+	/**
+	 * A new test user created for this test case.
+	 *
+	 * @var [WP_User]
+	 */
+	public static $test_user;
+
 
 	/**
-	 * Set up the test initially.
+	 * Set up the test case initially.
 	 */
 	public static function setUpBeforeClass() {
 		self::$user_class = new \WPDIU\User();
 		self::$diu_class  = new \WPDIU();
+
+		self::create_test_user();
+	}
+
+	/**
+	 * Clear the temporary data when the test case finishes.
+	 *
+	 * @return void
+	 */
+	public static function tearDownAfterClass() {
+		self::delete_test_user();
+	}
+
+	/**
+	 * Creates a test user.
+	 *
+	 * @return void
+	 */
+	public static function create_test_user() {
+
+		$user_email = 'unit_test_user@example.com';
+		$user_name  = 'Unit Test User';
+		$user_id    = username_exists( 'unit_test_user' );
+
+		if ( ! $user_id && false == email_exists( $user_email ) ) {
+			$random_password = wp_generate_password( 12, false );
+			$user_id = wp_create_user( $user_name, $random_password, $user_email );
+			
+			self::$test_user = get_user_by( 'id', $user_id );
+
+		} else {
+			fwrite(STDERR, __( 'User already exists.', 'wp-disable-inactive-users' ) );
+		}
+	}
+
+	/**
+	 * Deletes the test user.
+	 *
+	 * @return void
+	 */
+	public static function delete_test_user() {
+		wp_delete_user( self::$test_user->ID );
 	}
 
 	/**
